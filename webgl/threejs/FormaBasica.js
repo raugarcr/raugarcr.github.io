6e6c3789a85c4332c1999dcdae9cbe766fd00ffc
Raul Garcia <raucre44@gmail.com>
2020-11-01 21:51:59 +0100

    1

--- webgl/threejs/FormaBasica.js
+++ webgl/threejs/FormaBasica.js
@@ -9,9 +9,6 @@ Autor: Raúl García Crespo
 //Motor, escena y la camara
 var renderer, scene, camera;
 
-//Otras variables globales
-var esferaCubo, angulo = 0;
-
 //Acciones
 init();
 loadScene();
@@ -42,47 +39,22 @@ function loadScene(){
 	var material = new THREE.MeshBasicMaterial({color:'yellow', wireframe:true});
 
 	//Geometrias
-	var geocubo = new THREE.BoxGeometry(2,2,2);
-	var geosfera = new THREE.SphereGeometry(1,30, 30);
 
 
 	//Objetos
 	var cubo = new THREE.Mesh(geocubo, material);
-	//Orden por defecto de las tranformaciones: 1º escalado, 2º rotacion  y 3º traslacion. Escrito TRS en notación
 	cubo.rotation.y = Math.PI/4;
 	cubo.position.x = -1;
-	var esfera = new THREE.Mesh(geosfera, material);
-	esfera.position.x = 1;
 
-	//Objeto Contenedor
-	esferaCubo = new THREE.Object3D();
-	esferaCubo.position.y=0.5;
-	esferaCubo.rotation.y=angulo;
-
-	///Modelo Externo
-	//Modelos en clara.io o sketchlab
-	var loader = new THREE.ObjectLoader();
-	loader.load('models/soldado/soldado.json',
-		function(obj){
-			obj.position.set(0,1,0);
-			cubo.add(obj);
-		}
-	);
 
 	//Organización de la Escena
-	esferaCubo.add(cubo);
-	esferaCubo.add(esfera);
-	scene.add(new THREE.AxisHelper(3));
-	scene.add(esferaCubo);
-
+	scene.add(cubo);
 
 
 }
 
 function update(){
-	//Variación de la escena entre frames
-	angulo += Math.PI/100;
-	esferaCubo.rotation.y = angulo;
+
 }
 
 function render(){
